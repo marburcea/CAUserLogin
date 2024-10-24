1fa40942de8889c27fc4dad932975c4884d598d9
marburcea <maria.burcea@mail.utoronto.ca>
2024-10-24 14:23:38 -0400

    Task 2.1

--- src/main/java/use_case/login/LoginUserDataAccessInterface.java
+++ src/main/java/use_case/login/LoginUserDataAccessInterface.java
@@ -27,4 +27,9 @@ public interface LoginUserDataAccessInterface {
      */
     User get(String username);
 
+    /**
+     * New function.
+     * @param name name.
+     */
+    void setCurrentUser(String name);
 }
